df90f6b379321eb7d3ef666eb7769838fc5f0954
kamelya.kalouche <tug39573@temple.edu>
2020-04-11 20:18:41 +0000

    Added footage table

--- appJS/.next/static/runtime/webpack.js
+++ appJS/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3ae19baa4a207dddeb0f";
+/******/ 	var hotCurrentHash = "92be2e27b2e4257fe123";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
