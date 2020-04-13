95f58b100f9dba667feb9b9bb7729078bda26c00
shakthi.panneer <tug40828@temple.edu>
2020-04-13 03:29:48 +0000

    FULL VIDEO WORKING

--- appJS/.next/static/development/pages/video.js
+++ appJS/.next/static/development/pages/video.js
@@ -55497,13 +55497,6 @@ function video(props) {
 }
 
 ;
-/*function Video() {
-
-
-    return <video width="1280" height="720" controls  > <source src= 'http://0.0.0.0:8007/footage/' type="video/mp4"></source></video>
-  }
-  
-  export default Video*/
 
 /***/ }),
 
--- appJS/.next/static/runtime/webpack.js
+++ appJS/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8b48bfcf4ce7720bbbbf";
+/******/ 	var hotCurrentHash = "4f2726e2088e779fec37";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
